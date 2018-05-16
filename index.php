2fa46aba166f3ef802d9fcd22fcecb394f54594f
halbeisent <dev@halbeisen.fr>
2018-05-16 12:04:23 +0200

    Exercice 3 Partie 1 PHP Terminé et corrigé

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css">
+    <title>Exercice 3 Partie 1 PHP</title>
+  </head>
+  <body>
+  <?php
+  $km = 1;
+  echo $km . '<br />';
+  $km = 3;
+  echo $km . '<br />';
+  $km = 125;
+  echo $km . '<br />';
+  ?>
+  </body>
+</html>
